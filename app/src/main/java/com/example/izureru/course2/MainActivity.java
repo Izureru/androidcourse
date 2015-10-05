f69b5d23b39523ba5a437c0f17573d01494cf11b
izureru <israel.lewis@mnscorp.net>
2015-10-05 11:54:55 +0100

    two buttons working

--- app/src/main/java/com/example/izureru/course2/MainActivity.java
+++ app/src/main/java/com/example/izureru/course2/MainActivity.java
@@ -7,19 +7,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private TextView textView;
+    private EditText editText2;
+    private TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        TextView textView = (TextView)findViewById(R.id.textView);
+        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView2);
         textView.setText("Hello World");
+
+        editText = (EditText)findViewById(R.id.editText);
+        editText2 = (EditText)findViewById(R.id.editText2);
+
+
         Button button = (Button)findViewById(R.id.button);
+        Button button2 = (Button)findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,10 +39,19 @@ public class MainActivity extends AppCompatActivity {
                 doStuff();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("Tag", "Button2 Clicked");
+                doStuff();
+            }
+        });
     }
 
     private void doStuff() {
         Toast.makeText(MainActivity.this, "Button was Pressed", Toast.LENGTH_SHORT).show();
+        textView.setText(editText.getText() + " " + editText.getText());
+        textView2.setText(editText2.getText() + " " + editText2.getText());
     }
 
 }
