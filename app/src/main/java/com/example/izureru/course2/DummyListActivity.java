aab189d4b6bc2d67530d029042394a83dc0f1462
izureru <israel.lewis@mnscorp.net>
2015-10-06 10:22:36 +0100

    alternate cell colours and added address to person class

--- app/src/main/java/com/example/izureru/course2/DummyListActivity.java
+++ app/src/main/java/com/example/izureru/course2/DummyListActivity.java
@@ -1,5 +1,6 @@
 package com.example.izureru.course2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,20 +28,37 @@ public class DummyListActivity extends AppCompatActivity {
         setDummyData();
         lv.setAdapter(new ArrayAdapter<Person>(this, R.layout.custom_cell, R.id.textView5, dummyData)
         {
+
+            class ViewHolder{
+                TextView t;
+                TextView details;
+                ImageView iv;
+            }
+
             @Override
             public View getView(int position, View cell, ViewGroup parent) {
+                ViewHolder vh = null;
                 if (cell == null){
                     cell = getLayoutInflater().inflate(R.layout.custom_cell, parent, false);
+                    vh = new ViewHolder();
+                    vh.t = (TextView)cell.findViewById(R.id.textView5);
+                    vh.details = (TextView)cell.findViewById(R.id.textView3);
+                    vh.iv = (ImageView)cell.findViewById(R.id.imageView3);
+                    cell.setTag(vh);
+
                 }
-                TextView t = (TextView)cell.findViewById(R.id.textView5);
-                ImageView iv = (ImageView)cell.findViewById(R.id.imageView3);
+                else
+                {
+                    vh = (ViewHolder)cell.getTag();
+                }
 
                 Person p = dummyData.get(position);
-                t.setText(p.getName());
+                vh.t.setText(p.getName());
+                vh.details.setText(p.getAddress());
+                cell.setBackgroundColor((position%2 == 0) ? Color.GRAY: Color.GREEN);
                 return cell;
             }
         });
-//        lv.setAdapter(new ArrayAdapter<Person>(this, R.layout.custom_cell, R.id.textView5, dummyData));
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
