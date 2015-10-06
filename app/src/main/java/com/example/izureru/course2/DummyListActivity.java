ac7d8dfd99b46e2812857d058ab5137b36a81625
izureru <israel.lewis@mnscorp.net>
2015-10-06 10:03:51 +0100

    Lists pulling image and populating list

--- app/src/main/java/com/example/izureru/course2/DummyListActivity.java
+++ app/src/main/java/com/example/izureru/course2/DummyListActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -22,7 +25,22 @@ public class DummyListActivity extends AppCompatActivity {
         ListView lv = (ListView)findViewById(R.id.listView);
 
         setDummyData();
-        lv.setAdapter(new ArrayAdapter<Person>(this, R.layout.custom_cell, R.id.textView5, dummyData));
+        lv.setAdapter(new ArrayAdapter<Person>(this, R.layout.custom_cell, R.id.textView5, dummyData)
+        {
+            @Override
+            public View getView(int position, View cell, ViewGroup parent) {
+                if (cell == null){
+                    cell = getLayoutInflater().inflate(R.layout.custom_cell, parent, false);
+                }
+                TextView t = (TextView)cell.findViewById(R.id.textView5);
+                ImageView iv = (ImageView)cell.findViewById(R.id.imageView3);
+
+                Person p = dummyData.get(position);
+                t.setText(p.getName());
+                return cell;
+            }
+        });
+//        lv.setAdapter(new ArrayAdapter<Person>(this, R.layout.custom_cell, R.id.textView5, dummyData));
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
