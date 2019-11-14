3a4cf7e262c3ffba0586a7380270c3e9f9b405b2
izureru <israel.lewis@mnscorp.net>
2019-11-14 12:30:19 +0000

    changing master branch to test rebase

--- app/src/main/gen/com/example/izureru/course2/Manifest.java
+++ app/src/main/gen/com/example/izureru/course2/Manifest.java
@@ -5,3 +5,5 @@ package com.example.izureru.course2;
 /* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
 public final class Manifest {
 }
+
+//This comment serves no purpose except to show that master branch has changed
